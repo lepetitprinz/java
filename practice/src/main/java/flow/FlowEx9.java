9293a4a0f3661ec3f54a7f7bba5a4253525e8c62
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-11-22 02:55:30 +0900

    update source

---
+++ practice/src/main/java/flow/FlowEx9.java
@@ -0,0 +1,21 @@
+package flow;
+
+public class FlowEx9 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i=0; i<arr.length; i++) {
+            sum1 += arr[i];
+        }
+
+        for (int tmp : arr) {
+            sum2 += tmp;
+        }
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+
+    }
+}
