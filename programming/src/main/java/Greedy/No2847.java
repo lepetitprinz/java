d51cf067eb2751be9f6850f4f4dd810d87072379
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-12-04 00:30:22 +0900

    update source

---
+++ programming/src/main/java/Greedy/No2847.java
@@ -0,0 +1,30 @@
+package Greedy;
+
+import java.util.*;
+
+public class No2847 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i=0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int value = arr[n - 1];
+        int cnt = 0;
+
+        for (int i=n-2; i >= 0; i--) {
+            if (arr[i] >= value) {
+                cnt += arr[i] - value + 1;
+                value -= 1;
+            }
+            else {
+                value = arr[i];
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
