f1e615e3fdff3fc1d3dc3425fd6b5b0d517a6bb2
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-12-06 12:10:30 +0900

    update source

---
+++ programming/src/main/java/Greedy/No1246.java
@@ -0,0 +1,36 @@
+package Greedy;
+
+import java.io.IOException;
+import java.util.*;
+
+public class No1246 {
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        arr = new int[m];
+
+        for (int i=0; i < m; i++)
+            arr[i] = sc.nextInt();
+
+        Arrays.sort(arr);
+
+        int max_price = 0;
+        int max_sales = 0;
+
+        for (int i=0; i < m; i++) {
+            int sales = arr[i] * Math.min(n, m - i);
+            if (sales > max_sales) {
+                max_price = arr[i];
+                max_sales = sales;
+            }
+        }
+
+        System.out.printf("%d %d", max_price, max_sales);
+    }
+}
